f396a814485ed6c090afaee7daaaf41382cf9714
Shashi0010 <b.kanthshashi490@gmail.com>
2020-06-23 20:16:01 +0530

    Added PetTypes in Bootstrap. Closes #15

--- pet-clinic-web/src/main/java/com/zorin/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/zorin/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.zorin.petclinic.bootstrap;
 
 import com.zorin.petclinic.model.Owner;
+import com.zorin.petclinic.model.PetType;
 import com.zorin.petclinic.model.Vet;
 import com.zorin.petclinic.service.OwnerService;
+import com.zorin.petclinic.service.PetTypeService;
 import com.zorin.petclinic.service.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,26 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog =  new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
+        System.out.println("Pet Types Loaded");
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Shashi");
