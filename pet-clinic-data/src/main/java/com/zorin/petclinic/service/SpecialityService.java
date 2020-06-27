5816ecb056c7253d3be1c46cd31bca681309823b
Shashi0010 <b.kanthshashi490@gmail.com>
2020-06-27 23:00:54 +0530

    Created a SpecialityMapService Impl. Closes #14

---
+++ pet-clinic-data/src/main/java/com/zorin/petclinic/service/SpecialityService.java
@@ -0,0 +1,9 @@
+package com.zorin.petclinic.service;
+
+import com.zorin.petclinic.model.Speciality;
+
+/**
+ * Created by Chintu on 6/27/2020
+ */
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
