15f7fc5697e22475696a0a8d3da659601d447ed1
Shashi0010 <b.kanthshashi490@gmail.com>
2020-06-28 14:06:47 +0530

    Converted Visit to JPA Entitiy. Closes #21

--- pet-clinic-data/src/main/java/com/zorin/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/zorin/petclinic/model/Visit.java
@@ -1,13 +1,22 @@
 package com.zorin.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * Created by Chintu on 6/23/2020
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
