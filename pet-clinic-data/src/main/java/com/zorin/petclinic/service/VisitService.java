b8dbde6230405f8b82dae3107bfaf8014f15727f
Shashi0010 <b.kanthshashi490@gmail.com>
2020-07-03 20:15:17 +0530

    Created A Visit Map Service. Closes #28

---
+++ pet-clinic-data/src/main/java/com/zorin/petclinic/service/VisitService.java
@@ -0,0 +1,13 @@
+package com.zorin.petclinic.service;
+
+import com.zorin.petclinic.model.Visit;
+
+/**
+ * Created by Chintu on 7/3/2020
+ */
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+
+
+}
